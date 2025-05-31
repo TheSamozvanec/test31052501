a34c2949487716adc423b3629a6ab1b697946f7d
TheSamozvanec <semov777@mail.ru>
2025-05-31 16:24:51 +0300

    test

---
+++ dist/test.js
@@ -0,0 +1,3 @@
+"use strict";
+//test file
+console.log("hello!");
---
+++ src/test.ts
@@ -0,0 +1,32 @@
+//test file
+console.log("hello!");
+interface IUser {
+   name:string;
+   login:string;
+   level:'junior'|'medium'|'profi'; 
+   age:number;
+   target?:string;
+   greeting?:string;
+}
+let user01:IUser={
+    name:'Ivan',
+    login:'Vantuz',
+    level:'junior',
+    target:'GAME',
+    age:15
+}
+let obj={a:10,b:20,c:15}
+let i:keyof IUser;
+for (i in user01){
+    console.log(i+':'+user01[i]);
+}
+type fnstr = (x:number,y:number) => string;
+let fn1:fnstr = function(x,y){
+    let str:string=String(x*y);
+    let res:number=0;
+    for (let i=0;i<str.length;i++){
+        res+=Number(str[i]);
+    }
+    return String(res)
+}
+console.log(fn1(124,83));
