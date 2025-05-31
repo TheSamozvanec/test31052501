30f4310b4a5ee6536d1d353dc8d287b5c55b6795
TheSamozvanec <semov777@mail.ru>
2025-05-31 16:47:37 +0300

    refresh js file

--- dist/test.js
+++ dist/test.js
@@ -1,3 +1,25 @@
 "use strict";
 //test file
 console.log("hello!");
+let user01 = {
+    name: 'Ivan',
+    login: 'Vantuz',
+    level: 'junior',
+    target: 'GAME',
+    age: 15
+};
+let obj = { a: 10, b: 20, c: 15 };
+let i;
+for (i in user01) {
+    console.log(i + ':' + user01[i]);
+}
+let fn1 = function (x, y) {
+    let str = String(x * y);
+    let res = 0;
+    for (let i = 0; i < str.length; i++) {
+        res += Number(str[i]);
+    }
+    return String(res);
+};
+console.log(fn1(124, 83));
+// test new commit
